481675a6d1658debd9a2e7d6f790bbe08ff9543b
takashi <42316228+k-tokitoh@users.noreply.github.com>
2024-05-02 18:11:15 +0900

    画像を追加

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let src: string;
+	export let alt: string = '';
+</script>
+
+<div class="container">
+	<img {src} {alt} />
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+	}
+
+	img {
+		width: min(100%, 540px);
+	}
+</style>
